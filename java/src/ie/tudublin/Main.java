012c9dc7825de53f23444e6cc5eec94c7889f09d
Roxana-Pohodnicaru <C21446942@mytudublin.ie>
2023-02-20 12:17:08 +0000

    Audio beginning

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,3 +1,4 @@
+
 package ie.tudublin;
 
 public class Main
@@ -26,6 +27,12 @@ public class Main
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new StarMap());
     }
+
+	public static void audio1()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
 	
 	public static void main(String[] args)
 	{
@@ -35,7 +42,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		starMap();
+		audio1();
 	}
 	
 }
